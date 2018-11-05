82dd2643968fa8e19e141cbf4cb3889368aabbc6
KaZiUpl <38405516+KaZiUpl@users.noreply.github.com>
2018-11-05 19:10:29 +0100

    dodano tabele pracownicy

---
+++ USOSweb/first.sql
@@ -0,0 +1,44 @@
+DROP TABLE studenci;
+DROP TABLE zaliczenia;
+DROP TABLE grupy;
+DROP TABLE przedmioty;
+
+CREATE TABLE studenci
+(
+    id_studenta INT PRIMARY KEY,
+    imie VARCHAR(32),
+    nazwisko VARCHAR(32)
+);
+
+CREATE TABLE grupy
+(
+    id_grupy INT PRIMARY KEY,
+    numer INT,
+    przedmiot VARCHAR(255)
+);
+
+CREATE TABLE zaliczenia
+(
+    id_zaliczenia INT PRIMARY KEY,
+    id_studenta INT,
+    id_grupy INT,
+    ocena INT
+);
+
+CREATE TABLE przedmioty
+(
+    id_przedmiotu INT PRIMARY KEY,
+    nazwa VARCHAR(255),
+    typ_zaliczenia VARCHAR(50),
+    wymiar_godzin INT
+);
+
+CREATE TABLE pracownicy
+(
+	id_pracownika INT PRIMARY KEY,
+    id_grupy INT,
+	imie VARCHAR(32),
+	nazwisko VARCHAR(32),
+	typ_pracownika VARCHAR(32),
+	tytul VARCHAR(128)
+);
