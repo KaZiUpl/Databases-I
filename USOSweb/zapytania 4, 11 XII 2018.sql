b8238c28ad7ce3aa4dfa613063e22505a00773fd
KaZiUpl <38405516+KaZiUpl@users.noreply.github.com>
2018-12-11 09:10:08 +0100

    Update zapytania 4, 11 XII 2018.sql

--- USOSweb/zapytania 4, 11 XII 2018.sql
+++ USOSweb/zapytania 4, 11 XII 2018.sql
@@ -54,12 +54,12 @@ CREATE TABLE sprzedawcy (
     imie            VARCHAR(50),
     nazwisko        VARCHAR(80)
 );
-
 CREATE TABLE sprzedaz (
-    id_sprzedazy   INT PRIMARY KEY,
-    za_ile         DOUBLE PRECISION,
-    co             VARCHAR(60),
-    data           DATE
+    id_sprzedazy    INT PRIMARY KEY,
+    co              VARCHAR(60),
+    id_sprzedawcy   INT,
+    za_ile          DOUBLE PRECISION,
+    data            DATE
 );
 
 INSERT INTO SPRZEDAWCY VALUES ('1', 'Jan', 'Kowalski')
@@ -73,3 +73,21 @@ INSERT INTO SPRZEDAWCY VALUES ('8', '4', 'Karolina', 'Nowygraj');
 INSERT INTO SPRZEDAWCY VALUES ('9', '5', 'Ewa', 'Kac');
 INSERT INTO SPRZEDAWCY VALUES ('10', '5', 'Bartosz', 'Kuziemski');
 
+
+INSERT INTO SPRZEDAZ VALUES ('1', 'Proszek do prania', '1', '20', TO_DATE('2018-11-01 09:00:10', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO SPRZEDAZ VALUES ('2', 'Nożyczki', '1', '3', TO_DATE('2018-11-02 09:00:36', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO SPRZEDAZ VALUES ('3', 'Monitor', '2', '600', TO_DATE('2018-11-03 09:00:43', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO SPRZEDAZ VALUES ('4', 'Klepsydra', '3', '30', TO_DATE('2018-11-04 09:00:50', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO SPRZEDAZ VALUES ('5', 'Zegar', '4', '35', TO_DATE('2018-11-05 09:00:56', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO SPRZEDAZ VALUES ('6', 'Klawiatura', '5', '130', TO_DATE('2018-11-06 09:01:01', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO SPRZEDAZ VALUES ('7', 'Biurko', '5', '400', TO_DATE('2018-11-07 09:01:07', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO SPRZEDAZ VALUES ('8', 'Woda mineralna', '6', '1', TO_DATE('2018-11-08 09:01:11', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO SPRZEDAZ VALUES ('9', 'Farba czerwona', '6', '40', TO_DATE('2018-11-09 09:01:16', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO SPRZEDAZ VALUES ('10', 'Chusteczki', '7', '3', TO_DATE('2018-11-10 09:01:21', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO SPRZEDAZ VALUES ('11', 'Telewizor', '7', '1700', TO_DATE('2018-12-01 09:01:26', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO SPRZEDAZ VALUES ('12', 'Zeszyt w kratkę', '8', '2', TO_DATE('2018-12-02 09:01:30', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO SPRZEDAZ VALUES ('13', 'Diamenty', '9', '5000', TO_DATE('2018-12-03 09:01:34', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO SPRZEDAZ VALUES ('14', 'Rubiny', '10', '4000', TO_DATE('2018-12-04 09:01:38', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO SPRZEDAZ VALUES ('15', 'Szafiry', '10', '3000', TO_DATE('2018-12-05 09:01:41', 'YYYY-MM-DD HH24:MI:SS'));
+--=============================== Zadanie 2 ===============================
+-- znajdz zespol Jana Kowalskiego 
